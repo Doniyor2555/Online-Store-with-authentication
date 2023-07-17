165b6e04b1291119eb77fd52fe5bf01a0c023c65
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-07-17 16:48:46 +0500

    Online-Store

--- routes/auth.js
+++ routes/auth.js
@@ -20,5 +20,6 @@ router.post('/reset', authController.postReset);
 
 router.get('/reset/:token', authController.getNewPassword);
 
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
