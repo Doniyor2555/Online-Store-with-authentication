fa4d0c5ebf292fd73d07c93f4f00d342b0cb2cd9
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-07-19 11:26:25 +0500

    Added validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,6 +39,18 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      isAuthenticated: false,
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
@@ -71,7 +83,7 @@ exports.postSignup = (req, res, next) => {
   const confirmPassword = req.body.confirmPassword;
 
   const errors = validationResult(req);
-  
+
   if (!errors.isEmpty()) {
     return res.status(422).render('auth/signup', {
       path: '/signup',
@@ -80,36 +92,28 @@ exports.postSignup = (req, res, next) => {
     });
   }
 
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-mail exists already, please pick a different one.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            username: username,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login');
-          return transporter.sendMail({
-            from: "apisjscript@gmail.com",
-            to: email,
-            subject: "Hello",
-            text: 'Hello from Doni',
-            html: `<hr> <h1>Hello - ${username}</h1> <img src="https://images.unsplash.com/photo-1604066867775-43f48e3957d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="store"/> <p>You signed up successfully!</p>`
-          })
-        }).catch(err => console.log(err));
-    }).catch(err => {
-      console.log(err);
-    });
+
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        username: username,
+        cart: { items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+      return transporter.sendMail({
+        from: "apisjscript@gmail.com",
+        to: email,
+        subject: "Hello",
+        text: 'Hello from Doni',
+        html: `<hr> <h1>Hello - ${username}</h1> <img src="https://images.unsplash.com/photo-1604066867775-43f48e3957d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="store"/> <p>You signed up successfully!</p>`
+      })
+    }).catch(err => console.log(err));
 };
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { check, body } = require('express-validator')
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -10,17 +11,25 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login', [
+  body('email', 'Please enter a valid email address.').isEmail(), body('password', 'Password has to be valid.').isLength({ min: 5 }).isAlphanumeric()], authController.postLogin);
 
 router.post('/signup', [
   check('email')
     .isEmail()
     .withMessage('Please enter a valid email.')
     .custom((value, { req }) => {
-      if (value === 'test@test.com') {
-        throw new Error('This email address is forbidden.');
-      }
-      return true;
+      // if (value === 'test@test.com') {
+      //   throw new Error('This email address is forbidden.');
+      // }
+      // return true;
+
+      return User.findOne({ email: value })
+        .then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('E-mail exists already, please pick a different one.');
+          }
+        })
     }), body('password', 'Please enter a password with only numbers and text and at least 5 characters.')
       .isLength({ min: 5 })
       .isAlphanumeric(),
