526af4477e5fffcfd9331404a98abf9a54f6b2ff
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-07-19 10:47:32 +0500

    Added validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { check } = require('express-validator')
+const { check, body } = require('express-validator')
 
 const authController = require('../controllers/auth');
 
@@ -12,7 +12,26 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail() , authController.postSignup);
+router.post('/signup', [
+  check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email.')
+    .custom((value, { req }) => {
+      if (value === 'test@test.com') {
+        throw new Error('This email address is forbidden.');
+      }
+      return true;
+    }), body('password', 'Please enter a password with only numbers and text and at least 5 characters.')
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  body('confirmPassword').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Passwords have to match!');
+    }
+    return true;
+  })
+], authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
